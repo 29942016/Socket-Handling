45ca72276539680303680aab850a592a03cceb05
Oliver <obsta2010@hotmail.com>
2016-06-05 17:08:13 +0800

    seperated client code into this file

---
+++ files/client/clientAPI.py
@@ -0,0 +1,28 @@
+import socket
+
+host = socket.gethostname()
+port = 12345
+passkey = ""
+command = ""
+
+
+# 'Send a command
+def sendpacket(request):
+    try:
+        s = socket.socket()
+        s.connect((host,port))
+        s.send(request)
+        result = s.recv(1024)
+        s.close()
+        return result
+    except socket.error as serr:
+        if(serr.errno == 111):  # '111 - Host down
+            print 'Failed to connect, server may be unavailable.'
+            exit()
+
+# 'Password for sending and recieving commands, request will be hash+.+command
+def getpasskey():
+    global passkey
+    passkey = raw_input("Passkey: ")
+    passkey += '.'
+    return passkey
